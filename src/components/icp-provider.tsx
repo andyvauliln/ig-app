088e5c9c2c6e0abadc71a97b9714ba8758675267
Andrei <andy.vaulin@gmail.com>
2024-06-01 02:42:15 +0800

    added ICP provider

---
+++ src/components/icp-provider.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { ReactNode, useEffect } from "react";
+import { initSatellite } from "@junobuild/core-peer";
+
+export const ICPProvider = ({ children }: { children?: ReactNode }) => {
+    useEffect(() => {
+        // if (process.env.NEXT_PUBLIC_IS_ONCHAIN === 'true') {
+        (async () =>
+            await initSatellite({
+                satelliteId: process.env.NODE_ENV === "development" ? "jx5yt-yyaaa-aaaal-abzbq-cai" : process.env.NEXT_PUBLIC_SATTELITE_ID,
+                workers: {
+                    auth: true
+                }
+            }))();
+        // }
+    }, []);
+    return <>{children}</>
+}
