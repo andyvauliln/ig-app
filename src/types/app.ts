1d11c038c8fa872871b63c923d81e8aac2bbd730
Andrei <andy.vaulin@gmail.com>
2024-06-01 02:43:30 +0800

    added types

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,4 +1,5 @@
-import type {Doc} from '@junobuild/core-peer';
+import type { Doc } from '@junobuild/core-peer';
+import { IconKeys } from "@/components/icons"
 
 export interface NoteData {
   text: string;
@@ -6,3 +7,51 @@ export interface NoteData {
 }
 
 export type Note = Doc<NoteData>;
+
+export type HeroHeader = {
+  header: string
+  subheader: string
+  image: string
+}
+
+export type Content = {
+  text: string
+  subtext: string
+  icon?: IconKeys
+}
+
+export type ContentSection = {
+  header: string
+  subheader: string
+  image?: string
+  content: Array<Content>
+}
+
+export type SiteConfig = {
+  name: string
+  author: string
+  description: string
+  keywords: Array<string>
+  url: {
+    base: string
+    author: string
+  }
+  ogImage: string
+}
+
+export type ContactConfig = {
+  email: string
+}
+
+export type Settings = {
+  themeToggleEnabled: boolean
+}
+
+export type Layout = {
+  heroHeader: string
+  featureCards: string
+  headers: {
+    featureCards: string
+    features: string
+  }
+}
