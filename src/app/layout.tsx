1dd01365751aee113d854ba08f191879f95e73da
Andrei <andy.vaulin@gmail.com>
2024-06-01 02:43:20 +0800

    added home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
+import Navbar from "@/components/layout/navbar"
+import Footer from "@/components/layout/footer"
+import { AuthProvider } from "@/components/auth-provider";
+import { ICPProvider } from "@/components/icp-provider";
 import './globals.css';
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Imortal Games',
-  description: 'Educational AI Web 3 Platform'
-};
 
 export default function RootLayout({
   children
@@ -18,9 +17,17 @@ export default function RootLayout({
   return (
     <html lang="en" className='dark'>
       <body className={`${jetBrainsMono.className} bg-white dark:bg-black`}>
-        <Header />
-        {children}
-        <Footer />
+        <ICPProvider>
+          <AuthProvider>
+            <Navbar />
+            <div className="relative isolate min-h-[100dvh]">
+              <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24 tall:min-h-[calc(100dvh-128px)]">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </AuthProvider>
+        </ICPProvider>
       </body>
     </html>
   );
