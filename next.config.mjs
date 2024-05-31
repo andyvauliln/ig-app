ca51542b881e12c795efe9cdccdc9e84babecbfc
Andrei <andy.vaulin@gmail.com>
2024-05-31 22:28:34 +0800

    setup for react 19 next 15

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,38 @@ import { withJuno } from '@junobuild/nextjs-plugin';
 
 const isOnChain = process.env.NEXT_PUBLIC_IS_ONCHAIN === 'true';
 
-const config = isOnChain ? withJuno({ juno: { container: true } }) : {};
+const coreConfig = {
+    experimental: {
+        reactCompiler: true,
+        after: true,
+        // staleTimes: {
+        //     dynamic: 30,
+        //     static: 180
+        // },
+        // Automatically bundle external packages in the Pages Router:
+
+        // Opt specific packages out of bundling for both App and Pages Router:
+
+        //optimizePackageImports: ['icon-library'],
+
+    },
+    bundlePagesRouterDependencies: true,
+    images: {
+        remotePatterns: [{ hostname: "utfs.io" }],
+    },
+    // profiler: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    juno: { container: true }
+
+
+    // output: 'standalone'
+};
+
+const config = isOnChain ? withJuno(coreConfig) : { coreConfig };
 
 export default config;
