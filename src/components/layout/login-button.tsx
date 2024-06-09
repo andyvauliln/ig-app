b5e694047eed6d7d5f6024fe4fe3f3db9766e13a
Andrei <andy.vaulin@gmail.com>
2024-06-09 10:11:30 +0800

    changed login logout buttons

--- src/components/layout/login-button.tsx
+++ src/components/layout/login-button.tsx
@@ -1,6 +1,16 @@
-import { Button } from '@/components/archive/button';
 import { signIn } from '@junobuild/core-peer';
 
-export const Login = () => {
-  return <Button onClick={signIn}>Sign in</Button>;
-};
+export function Login() {
+  const handleSignIn = () => {
+    signIn();
+  };
+
+  return (
+    <button onClick={handleSignIn} className="p-[3px] relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2 min-w-36 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+        Log in
+      </div>
+    </button>
+  );
+}
--- src/components/layout/logout-button.tsx
+++ src/components/layout/logout-button.tsx
@@ -1,22 +1,13 @@
-import {signOut} from '@junobuild/core-peer';
+import { signOut } from '@junobuild/core-peer';
 
-export const Logout = () => {
+
+export function Logout() {
   return (
-    <button
-      type="button"
-      onClick={signOut}
-      className="dark:text-white flex items-center gap-2 mt-24 hover:text-lavender-blue-500 active:text-lavender-blue-400">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="16"
-        viewBox="0 -960 960 960"
-        width="16"
-        fill="currentColor">
-        <path d="M120-120v-720h360v80H200v560h280v80H120Zm520-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
-      </svg>
-      <span>
-        <small>Logout</small>
-      </span>
+    <button onClick={signOut} className="p-[3px] relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+        Sign up
+      </div>
     </button>
-  );
-};
+  )
+}
