7756e3be6b9281877d050df0ac813ee5a94142b6
Andrei <andy.vaulin@gmail.com>
2024-06-09 10:12:14 +0800

    added header component v2

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,11 @@ import { AuthContext } from '@/components/layout/providers/auth-provider';
 import { Login } from '@/components/layout/login-button';
 import { Logout } from '@/components/layout/logout-button';
 import Image from "next/image"
+import AnimatedLogo from "@/components/layout/animated-logo"
+import { Anton } from "next/font/google"
+import { usePathname } from 'next/navigation'
 
+const anton = Anton({ subsets: ["latin"], weight: "400", style: "normal" });
 interface NavbarLinksProps {
   navbar: boolean
   handleClick: () => void
@@ -21,7 +25,6 @@ interface NavbarToggleProps {
   navbar: boolean
   setNavbar: Dispatch<SetStateAction<boolean>>
 }
-
 
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false)
@@ -43,28 +46,29 @@ export default function Navbar() {
   return (
     <header className="select-none">
       <nav className="mx-auto justify-between px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
-        <div>
-          <div className="flex items-center justify-between py-3 md:block md:py-5">
-            <Link href="/" onClick={handleClick} className="flex items-center space-x-2 lg:hover:scale-[1.10]">
-              <Image src='/logo.png' alt={siteConfig.name} className="" width={32} height={32} />
-              <span className="text-2xl font-bold duration-200 text-secondary">
-                {siteConfig.name}
-              </span>
+        <div className="flex items-center justify-between py-3 md:block md:py-5">
+          <div className="flex items-center w-full">
+            <Link href="/" className="flex items-center space-x-2 lg:hover:scale-[1.10]">
+              <Image src='/logo.svg' alt={siteConfig.name} className="text-secondary" width={60} height={60} />
+              {/* <CompanyName /> */}
             </Link>
-            <div className="flex gap-1 md:hidden">
-              <NavbarToggle navbar={navbar} setNavbar={setNavbar} />
-              <ModeToggle />
-            </div>
           </div>
+          <div className="flex gap-1 md:hidden">
+            {user ? <Logout /> : <Login />}
+          </div>
+          {/*
+            {user ? <Logout /> : <Login />}
+            <NavbarToggle navbar={navbar} setNavbar={setNavbar} />
+         */}
         </div>
         <NavbarLinks navbar={navbar} handleClick={handleClick} />
-        <div className="flex items-center space-x-4">
-          {user ? <Logout /> : <Login />}
-          {settings.themeToggleEnabled && (
+        <div className="hidden md:flex items-center space-x-4">
+          {!navbar && user ? <Logout /> : <Login />}
+          {/* {settings.themeToggleEnabled && (
             <div className="hidden md:block">
               <ModeToggle />
             </div>
-          )}
+          )} */}
         </div>
       </nav>
     </header>
@@ -74,7 +78,7 @@ export default function Navbar() {
 export function NavbarToggle({ navbar, setNavbar }: NavbarToggleProps) {
   return (
     <button
-      className="rounded-md p-2 text-primary outline-none focus:border focus:border-primary"
+      className="ml-2 rounded-md p-2 text-azure outline-none border-[3px] border-purple-500"
       aria-label="Hamburger Menu"
       onClick={() => setNavbar(!navbar)}
     >
@@ -83,7 +87,7 @@ export function NavbarToggle({ navbar, setNavbar }: NavbarToggleProps) {
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
           viewBox="0 0 20 20"
-          fill="currentColor"
+          fill="rgb(168 85 247)"
         >
           <path
             fillRule="evenodd"
@@ -97,7 +101,7 @@ export function NavbarToggle({ navbar, setNavbar }: NavbarToggleProps) {
           className="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
+          stroke="rgb(168 85 247)"
           strokeWidth={2}
         >
           <path
@@ -111,27 +115,73 @@ export function NavbarToggle({ navbar, setNavbar }: NavbarToggleProps) {
   )
 }
 
-export function NavbarLinks({ navbar, handleClick }: NavbarLinksProps) {
+
+
+function NavbarLinks({ navbar, handleClick }: NavbarLinksProps) {
+  const pathname = usePathname()
+  console.log("PATHNAME", pathname)
   return (
-    <div
-      className={`absolute left-0 right-0 z-10 m-auto justify-self-center rounded-md border bg-background p-4 md:static md:mt-0 md:block md:border-none md:p-0 ${navbar ? "block" : "hidden"
-        }`}
-      style={{ width: "100%", maxWidth: "20rem" }}
-    >
-      <ul className="flex flex-col items-center space-y-4 text-primary opacity-60 md:flex-row md:space-x-6 md:space-y-0">
+    <div className={`flex items-center justify-center m-auto rounded-md p-4`}>
+      <ul className="buttons scale-100 md:scale-[1.5] lg:scale-[2]">
         {navLinks.map((link) => (
-          <li key={link.route}>
-            <Link
-              className="hover:underline"
-              href={link.path}
-              onClick={handleClick}
-            >
+          link.path !== '/' || pathname !== '/' ? (
+            <NavButton3 key={link.route} path={link.path} isActive={pathname === link.path}>
               {link.route}
-            </Link>
-          </li>
+            </NavButton3>
+            // <li key={link.route} className="flex items-center">
+            //   <Link
+            //     href={link.path}
+            //     onClick={handleClick}
+            //   >
+            //     {link.route}
+            //   </Link>
+            // </li>
+          ) : null
         ))}
       </ul>
     </div>
   )
 }
 
+function CompanyName() {
+  return (
+    <h1 className={`${anton.className} mt-4 bg-gradient-to-br from-slate-300 to-slate-500  bg-clip-text text-center text-6xl font-medium tracking-tight text-transparent md:text-7xl`}>
+      Histoverse
+    </h1>
+
+  )
+}
+
+function NavButton2({ children, path }: { children: React.ReactNode, path: string }) {
+  return (
+    <Link href={path} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        {children}
+      </span>
+    </Link>
+  );
+}
+// function NavButton({ children, path }: { children: React.ReactNode, path: string }) {
+//   return (
+//     <Link href={path} className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+//       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+//       <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+//         {children}
+//       </span>
+//     </Link>
+//   );
+// }
+
+function NavButton3({ children, path, isActive }: { children: React.ReactNode, path: string, isActive: boolean }) {
+  return (
+    <a href={path} className={`${isActive ? "active" : ""}`}>
+      {children}
+    </a>
+  )
+}
+
+
+{/* <a class='glowBtn'>Button</a>
+    <a class='glowBtn hover'>Hover</a>
+    <a class='glowBtn active'>Active</a> */}
