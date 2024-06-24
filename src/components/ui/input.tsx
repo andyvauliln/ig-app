c7cd93da0ca0ec29cc2822a34288812a009a07f1
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:29:00 +0800

    added yellow input and textarea

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -26,23 +26,24 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = "Input"
 
+
 export { Input, GradientInput, GradientTextArea }
 
 const GradientInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, placeholder, type = "text", ...props }, ref) => {
     return (
-      <div className="my-4 p-[1.5px] rounded-md max-w-sm bg-gradient-to-r via-sky-400 from-purple-500 to-purple-500">
-        {/* <label htmlFor="name" className="sr-only">Name</label> */}
-        <input placeholder={placeholder}  {...props} ref={ref} className="px-3 py-2 w-full flex h-10 text-sm  ring-offset-background rounded-md bg-background text-muted-foreground placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type={type} placeholder="Enter Your Name" />
-      </div>
+      <div className={cn(className, "p-[1.5px] rounded-md bg-gradient-to-r from-yellow-700 via-gray-900 to-yellow-500", className)}>
+        <input placeholder={placeholder} {...props} ref={ref} className="px-3 py-2 w-full flex h-10 text-sm  ring-offset-background rounded-md bg-background text-muted-foreground placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type={type} />
+      </div >
     )
   }
 )
+GradientInput.displayName = "GradientInput"
 
 const GradientTextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   ({ className, placeholder, ...props }, ref) => {
     return (
-      <div className="my-4 p-[1.5px] rounded-md max-w-sm bg-gradient-to-r via-sky-400 from-purple-500 to-purple-500">
+      <div className="my-4 p-[1.5px] w-full rounded-md bg-gradient-to-r from-yellow-500 via-yellow-700 to-yellow-500">
         <textarea
           className="px-3 py-2 w-full flex h-40 text-sm ring-offset-background rounded-md bg-background text-muted-foreground placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={placeholder}
@@ -53,3 +54,4 @@ const GradientTextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
     )
   }
 )
+GradientTextArea.displayName = "GradientTextArea"
