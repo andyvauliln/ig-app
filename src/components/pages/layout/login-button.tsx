879fb538be72954b1edbd1d79a68967997d0d702
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:27:51 +0800

    switch to yellow button

--- src/components/pages/layout/login-button.tsx
+++ src/components/pages/layout/login-button.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { signIn } from '@junobuild/core-peer';
+import { YellowButton } from '@/components/ui/button';
 
 export function Login() {
   const handleSignIn = () => {
@@ -7,12 +8,9 @@ export function Login() {
   };
 
   return (
-    <button onClick={handleSignIn} className="relative p-1 bg-black text-white rounded-md group overflow-hidden">
-      <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(/old_gold.jpeg)' }}></div>
-      <div className="relative z-10 px-8 font-extrabold py-2 bg-black rounded-md group-hover:text-yellow-500 text-shadow-yellow-500 transition duration-200">
-        Log in
-      </div>
-      <div className="absolute inset-0 border-4 border-transparent rounded-md pointer-events-none" style={{ clipPath: 'inset(0 0 0 0 round 6px)', backgroundImage: 'url(/old_gold.jpeg)' }}></div>
-    </button>
+    <YellowButton onClick={handleSignIn}>
+      Login
+    </YellowButton>
+
   );
 }
