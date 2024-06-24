8b8d4a37c4dedd89c4a0b50d91d035c7b94e42dd
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:23:54 +0800

    added create conten page

--- src/app/(pages)/create-content/page.tsx
+++ src/app/(pages)/create-content/page.tsx
@@ -14,7 +14,7 @@ import Sidebar from "@/components/pages/create-lessons-page/Sidebar";
 export default function CreateLessonPage() {
     return (
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
-            <Sidebar />
+            {/* <Sidebar />
             <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                 <Header />
                 <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
@@ -35,7 +35,7 @@ export default function CreateLessonPage() {
                         <SubHeaderRight />
                     </div>
                 </main>
-            </div>
+            </div> */}
         </div>
     );
 }
