b7a63c2fde949a3e992bb490916c6bb516ef02ff
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:24:27 +0800

    added request content page

--- src/app/(pages)/request-lesson/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function RequestLessonPage() {
-    return (
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-
-        </div>
-    );
-}
