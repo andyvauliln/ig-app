c4ab08465a220ab9a4c9d22568ee4d6413128c13
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:24:03 +0800

    added learn page

--- src/app/(pages)/learn/page.tsx
+++ src/app/(pages)/learn/page.tsx
@@ -1,8 +1,174 @@
+'use client'
 import React from "react"
+import { Bebas_Neue } from "next/font/google"
+import { YellowButton } from "@/components/ui/button";
+import { LayoutGrid } from "@/components/ui/bento-grid";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge"
+import { purple } from "tailwindcss/colors";
+import { GradientInput } from "@/components/ui/input";
+
+const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: "400", style: "normal" });
+
+
 export default function LearnPage() {
     return (
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <div className="flex justify-between min-h-screen w-full flex-col overflow-y-hidden">
+            <div className="px-4">
+                <h3 className={`${bebasNeue.className} text-yellow-500 text-xl lg:text-4xl`}> Learn</h3 >
+                <div className="flex w-full justify-between mb-6">
+                    <GradientInput className="w-[50%]" />
+                    <div className="space-x-2">
+                        <YellowButton onClick={() => { }}>
+                            Watched
+                        </YellowButton>
+                        <YellowButton onClick={() => { }}>
+                            Saved
+                        </YellowButton>
+                    </div>
+
+                </div>
+            </div>
+            <LayoutGrid cards={items} />
+        </div>
+
+    );
+}
+
+
+
+
+// const Skeleton = ({ url, alt }: { url: string, alt: string }) => (
+//     <div className="relative flex flex-1 w-full h-full min-h-[16rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+//         <Image src={url} alt={alt} layout="fill" objectFit="cover" className="rounded-xl" />
+//     </div>
+// );
+const Footer = ({ reward, numberOfVotes }: { reward: string, numberOfVotes: number }) => {
+    return <div className="flex justify-between mt-2 items-center">
+        <div className="flex items-center space-x-2">
+            <Badge variant="purple">Reward: {reward} ICP</Badge>
+            <Badge className="flex items-center bg-teal-500">
+                <i className="text-white fa fa-thumbs-up mr-1"></i>
+                <span className="text-white">{numberOfVotes}</span>
+            </Badge>
+
+        </div>
+        <YellowButton onClick={() => { }}>Upvote</YellowButton>
+    </div>
+};
+const OpenContent = ({ card = null }: { card?: any }) => {
+    return (
+        <div className="h-full w-full text-white">
+            A serene and tranquil retreat, this house in the woods offers a peaceful
+            escape from the hustle and bustle of city life.
+            A serene and tranquil retreat, this house in the woods offers a peaceful
+            escape from the hustle and bustle of city life.
+            A serene and tranquil retreat, this house in the woods offers a peaceful
+            escape from the hustle and bustle of city life.
+            A serene and tranquil retreat, this house in the woods offers a peaceful
+            escape from the hustle and bustle of city life.
+            A serene and tranquil retreat, this house in the woods offers a peaceful
+            escape from the hustle and bustle of city life.
 
         </div>
     );
-}
+};
+const items = [
+    {
+        id: 1,
+        title: "Ancient Civilizations",
+        description: "Delve into the history of ancient cultures and their contributions.",
+        header: null,
+        img_url: "/images/event_1.png",
+        footer: <Footer reward="100/400" numberOfVotes={100} />,
+        content: <OpenContent />,
+        date: "2021-01-01"
+    },
+    {
+        id: 2,
+        title: "The Age of Exploration",
+        description: "Discover the voyages and discoveries that shaped the modern world.",
+        header: null,
+        img_url: "/images/event_2.png",
+        footer: <Footer reward="300/400" numberOfVotes={100} />,
+        content: <OpenContent />,
+        date: "2023-01-01"
+    },
+    {
+        id: 3,
+        title: "The Industrial Revolution",
+        description: "Learn about the innovations that transformed industry and society.",
+        header: null,
+        img_url: "/images/event_3.png",
+        footer: <Footer reward="400/400" numberOfVotes={100} />,
+        content: <OpenContent />,
+        date: "2023-02-07"
+    },
+    {
+        id: 4,
+        title: "World War I and II",
+        description: "Understand the causes and consequences of the two global conflicts.",
+        header: null,
+        img_url: "/images/event_4.png",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        content: <OpenContent />,
+        date: "2024-06-07"
+    },
+    {
+        id: 5,
+        title: "The Cold War",
+        description: "Explore the tensions and rivalries that defined the post-war era.",
+        header: null,
+        img_url: "/images/event_5.png",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        content: <OpenContent />,
+        date: "2024-03-09"
+    },
+    {
+        id: 6,
+        title: "The American Revolution",
+        description: "Study the birth of the United States and its struggle for independence.",
+        header: null,
+        img_url: "/images/event_6.png",
+        content: <OpenContent />,
+        footer: <Footer reward="0/500" numberOfVotes={100} />,
+        date: "2024-06-09"
+    },
+    {
+        id: 7,
+        img_url: "/images/event_7.png",
+        title: "The Renaissance",
+        content: <OpenContent />,
+        description: "Witness the rebirth of art, science, and culture in Europe.",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        date: "2024-09-12"
+    },
+    {
+        id: 8,
+        img_url: "/images/event_8.png",
+        title: "The Renaissance",
+        content: <OpenContent />,
+        description: "Witness the rebirth of art, science, and culture in Europe.",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        date: "2024-09-12"
+    },
+    {
+        id: 9,
+        img_url: "/images/event_9.png",
+        title: "The Renaissance",
+        content: <OpenContent />,
+        description: "Witness the rebirth of art, science, and culture in Europe.",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        date: "2024-09-12"
+    },
+    {
+        id: 10,
+        img_url: "/images/event_10.png",
+        title: "The Renaissance",
+        content: <OpenContent />,
+        description: "Witness the rebirth of art, science, and culture in Europe.",
+        footer: <Footer reward="100" numberOfVotes={100} />,
+        date: "2024-09-12"
+    },
+];
+
