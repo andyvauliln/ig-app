39fdb91f66bd5b8c7102e840af3d8cd9519e05ed
Andrei <andy.vaulin@gmail.com>
2024-06-24 23:29:18 +0800

    update route config

--- src/config/route.ts
+++ src/config/route.ts
@@ -3,7 +3,8 @@ export const routes = {
     login: "/login",
     logout: "/logout",
     onboarding: "/onboarding",
-    createContent: "/create-content",
+    createContent: "/create-lesson",
     learn: "/learn",
+    requestLesson: "/request-lesson",
 }
 
