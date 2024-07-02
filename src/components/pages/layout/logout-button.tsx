76749c880cc3c31da11398451eed8e8cc470789f
Andrei <andy.vaulin@gmail.com>
2024-07-02 10:40:50 +0800

    logout button update

--- src/components/pages/layout/logout-button.tsx
+++ src/components/pages/layout/logout-button.tsx
@@ -1,14 +1,13 @@
 "use client"
 import { signOut } from '@junobuild/core-peer';
+import { YellowButton } from '@/components/ui/button';
 
 
 export function Logout() {
   return (
-    <button onClick={signOut} className="p-[3px] relative">
-      <div className="absolute inset-0 bg-gradient-to-r from-purple-600 to-sky-400 rounded-lg" />
-      <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
-        Sign up
-      </div>
-    </button>
+    <YellowButton onClick={signOut}>
+      Log Out
+    </YellowButton>
+
   )
 }
